344cf3cf5ef157c632859d67a4a6ead5f735bbe6
Abdul <140862207+abdul2811@users.noreply.github.com>
2024-01-01 14:10:35 -0500

    added error handling

--- index.js
+++ index.js
@@ -128,14 +128,14 @@ app.post('/api/persons', (request, response) => {
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
-  const body = request.body
+  const {name, number} = request.body
 
   const person = {
     name: body.name,
     number: body.number,
   }
 
-  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+  Person.findByIdAndUpdate(request.params.id, {name, number}, { new: true, runValidators: true, context: 'query' })
     .then(updatedNote => {
       response.json(updatedNote)
     })
@@ -154,7 +154,9 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
-  } 
+  } else if (error.name === "ValidationError") {
+    return response.status(400).json({error: error.message})
+  }
 
   next(error)
 }
