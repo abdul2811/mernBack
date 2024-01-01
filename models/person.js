055db0c6661db2cf743ab6f22e64451d93983f64
Abdul <140862207+abdul2811@users.noreply.github.com>
2024-01-01 14:16:21 -0500

    added to schema

--- models/person.js
+++ models/person.js
@@ -18,8 +18,15 @@ mongoose.connect(url)
   })
 
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
+    name: {
+        type: String,
+        required: true
+      },
+      number: {
+        type: String,
+        required:true,
+        minLength: 3,
+      } 
 })
 
 personSchema.set('toJSON', {
